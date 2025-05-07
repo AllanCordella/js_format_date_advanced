e4a7a642ba8043f8afb32b45f2aa282b71b9fb3e
Allan Cordella <allancordella@gmail.com>
2025-05-07 18:14:49 -0300

    solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,71 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const newDate = [];
+  const separator = fromFormat.pop();
+  const joiner = toFormat.pop();
+  const explodedDate = date.split(separator);
+  let day = 0;
+  let year = 0;
+  let yearShort = 0;
+  let yearLong = 0;
+  let month = 0;
+  let finalDate = '';
+
+  for (let i = 0; i <= fromFormat.length; i++) {
+    if (fromFormat[i] === 'DD') {
+      day = explodedDate[i];
+    }
+
+    if (fromFormat[i] === 'MM') {
+      month = explodedDate[i];
+    }
+
+    if (fromFormat[i] === 'YY') {
+      year = explodedDate[i];
+    }
+
+    if (fromFormat[i] === 'YYYY') {
+      year = explodedDate[i];
+    }
+  }
+
+  if (year.length > 3) {
+    yearLong = year;
+    yearShort = year.slice(2, 4);
+  }
+
+  if (year.length < 3 && year >= 30) {
+    yearLong = 19 + year;
+    yearShort = year;
+  }
+
+  if (year.length < 3 && year < 30) {
+    yearLong = 20 + year;
+    yearShort = year;
+  }
+
+  for (let i = 0; i < toFormat.length; i++) {
+    if (toFormat[i] === 'DD') {
+      newDate[i] = day;
+    }
+
+    if (toFormat[i] === 'MM') {
+      newDate[i] = month;
+    }
+
+    if (toFormat[i] === 'YY') {
+      newDate[i] = yearShort;
+    }
+
+    if (toFormat[i] === 'YYYY') {
+      newDate[i] = yearLong;
+    }
+  }
+
+  finalDate = newDate.join(joiner);
+
+  return finalDate;
 }
 
 module.exports = formatDate;
